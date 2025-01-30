f361cb644e83d615a5bc7e66679374415fe1bcde
kwena <kwenakomape2@gmail.com>
2025-01-30 13:02:19 +0200

    added

--- vite.config.ts
+++ vite.config.ts
@@ -27,15 +27,25 @@
 //   },
 // });v
 
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import path from 'path';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, './src'),
+//     },
+//   },
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+  build: {
+    outDir: 'dist', // Ensure the build output goes to the dist folder
   },
 });
